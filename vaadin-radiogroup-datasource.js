1e21a77dc21cce7d7e4441599cdf285a9463bf2a
varunmehta1986 <mehta.varun.vm@gmail.com>
2020-02-05 23:45:27 -0600

    using unsafeHTML ro render

--- vaadin-radiogroup-datasource.js
+++ vaadin-radiogroup-datasource.js
@@ -1,4 +1,5 @@
 import { LitElement, html } from "lit-element";
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import '@vaadin/vaadin-radio-button/vaadin-radio-group';
 import '@vaadin/vaadin-radio-button/vaadin-radio-button';
 
@@ -6,32 +7,27 @@ class VaadinRadiogroupDataSource extends LitElement {
     static get properties() {
         return {
             radioGroupItems: { type: Array },
-            selectedGrocery : {type:String}
+            selectedGrocery: { type: String }
         }
     }
     constructor() {
         super();
         this.radioGroupItems = ["Milk", "Bananas", "Eggs"];
-        this.updateComplete.then(() => {
-            var radioGroup = this.shadowRoot.querySelector("vaadin-radio-group");
-            //in case you want to have a default checked None option.
-            var radioGroupList = "<vaadin-radio-button value='None' checked>None</vaadin-radio-button>";
-            for (let i = 0; i < this.radioGroupItems.length; i++) {
-                radioGroupList += "<vaadin-radio-button value = '" + this.radioGroupItems[i] + "'>" +
-                    this.radioGroupItems[i] + "</vaadin-radio-button>";
-            }
-            radioGroupList+= ""
-            radioGroup.innerHTML += radioGroupList;
-        });
     }
     render() {
+        var radioGroupList = "";
+        for (let i = 0; i < this.radioGroupItems.length; i++) {
+            radioGroupList += "<vaadin-radio-button value = '" + this.radioGroupItems[i] + "'>" +
+                this.radioGroupItems[i] + "</vaadin-radio-button>";
+        }
         return html`
-
             <vaadin-radio-group theme="vertical" @value-changed= "${this.radioValueChanged}">
+                <vaadin-radio-button value='None' checked id="vrbNone">None</vaadin-radio-button> 
+                 ${unsafeHTML(radioGroupList)}
             </vaadin-radio-group>
         `;
     }
-    radioValueChanged(e){
+    radioValueChanged(e) {
         this.selectedGrocery = e.target.value;
     }
 }
